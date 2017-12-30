1ca52790a15ee3fd2cde3c62d73600412598d7b6
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2017-12-31 00:06:54 +0100

    Day 6, minor refactoring to prevent use of global variables

--- 6.py
+++ 6.py
@@ -1,9 +1,6 @@
 from util import readinput, integers
 from functools import partial
 
-grid = [[0 for j in range(1000)] for i in range(1000)]
-grid2 = [[0 for j in range(1000)] for i in range(1000)]
-
 ops = {
     'on': (lambda x: 1),
     'off': (lambda x: 0),
@@ -24,10 +21,6 @@ def switch_lights(data, grid, ops):
             grid[i][j] = ops[instr](grid[i][j])
 
 
-switch_lights1 = partial(switch_lights, grid=grid, ops=ops)
-switch_lights2 = partial(switch_lights, grid=grid2, ops=ops2)
-
-
 def parse(data):
     d = data.split(' ')
     ranges = integers(data)
@@ -36,19 +29,30 @@ def parse(data):
     return 'toggle', ranges
 
 
+def solve1():
+    grid = [[0 for j in range(1000)] for i in range(1000)]
+    switch_lights1 = partial(switch_lights, grid=grid, ops=ops)
+    map(switch_lights1, readinput(6))
+    print(sum(map(sum, grid)))
+
+
+def solve2():
+    grid = [[0 for j in range(1000)] for i in range(1000)]
+    switch_lights2 = partial(switch_lights, grid=grid, ops=ops2)
+    map(switch_lights2, readinput(6))
+    print(sum(map(sum, grid)))
+
+
 def tests():
     assert parse('turn on 0,0 through 999,999') == ('on', (0, 0, 999, 999))
     assert parse('toggle 0,0 through 999,0') == ('toggle', (0, 0, 999, 0))
     assert parse('turn off 499,499 through 500,500') == ('off', (499, 499, 500,
                                                                  500))
-    assert sum(map(sum, grid)) == 0
+    assert sum(map(sum, [[0 for j in range(10)] for i in range(10)])) == 0
     print('tests pass')
 
 
 tests()
 
-map(switch_lights1, readinput(6))
-print(sum(map(sum, grid)))
-
-map(switch_lights2, readinput(6))
-print(sum(map(sum, grid2)))
+solve1()
+solve2()
